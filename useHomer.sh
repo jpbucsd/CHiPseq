4ff7f39e9bf1705443318f7d54b545e7626a3ff1
Jordan <jpburkhardt@ucsd.edu>
2022-05-30 19:21:50 -0700

    fixed path errors in useHomer.sh

--- useHomer.sh
+++ useHomer.sh
@@ -7,14 +7,14 @@ mkdir tagDirs
 while read line; \
 do
 	#make tag directory
-        dir/makeTagDirectory tagDirs/${line} ${line}.bam
+        bin/makeTagDirectory tagDirs/${line} ${line}.bam
 	#produce a bed graph using the directory
-	dir/makeUCSCfile tagDirs/${line} -o auto
+	bin/makeUCSCfile tagDirs/${line} -o auto
 done < SraAccList.txt
 
 #identify binding sites/peaks
 #as the original paper identifies promoter regions and does not look for histone modifications we will be using the factors style for the homer commands
 while read line; \
 do
-	dir/findPeaks tagdirs/${prefix} -i tagdirs/input -style factor -o auto
+	bin/findPeaks tagdirs/${prefix} -i tagdirs/input -style factor -o auto
 done < SraAccList.txt
